af1d5975ab41d01762201a6d1e771628a7b22258
IbrahAbd <ibrahim.abdou2121@gmail.com>
2024-12-22 12:01:50 +0200

    Extra comments added for more clarity.

--- MNIST.py
+++ MNIST.py
@@ -18,22 +18,21 @@ x_train = x_train / 255.0
 x_test = x_test / 255.0
 
 # Build neural network 
-
 model = Sequential([
     # Input layer of 28x28 pixels (784 total pixels / inputs)
     Flatten(input_shape =(28,28)),
 
     # Hidden layer of 128 neurons with relu activation function.
-    Dense(128, activation='relu'),
+    Dense(128, activation='sigmoid'),
 
     # Output layer of 10 neurons making use of the softmax activation function to assign probabilities to values.
     Dense(10, activation='softmax')
 ])
 
-
+# Specify a classifer and optimiser for the classifier.
 model.compile(
     # Optimiser value for Stochastic Gradient Descent with learning rate of 0.01.
-    optimizer = 'adam',#SGD(learning_rate = 0.01),
+    optimizer = SGD(learning_rate = 0.01),
 
     # Make use of cross entropy loss.
     loss = 'sparse_categorical_crossentropy',
@@ -43,18 +42,21 @@ model.compile(
 
 # Train the model
 
-history = model.fit(x_train, y_train, epochs = 10, validation_split=0.2, batch_size = 32)
+# Have 10 Epochs.
+# Make the batch size 32.
+# Validation split of dataset is 20% compared to the 80% used for training.
+
+history = model.fit(x_train, y_train, epochs = 10, validation_split = 0.2, batch_size = 32)
 
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
 
 predictions = model.predict(x_test)
 
-# Display a test image and its prediction
+# Display a test image and its prediction.
 
 for i in range(10,15):
     plt.imshow(x_test[i], cmap='gray')
     plt.title(f'Predicted: {np.argmax(predictions[i])}, True: {y_test[i]}')
     plt.axis('off')
     plt.show()
-
